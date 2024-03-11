ac982c67411979f14a3ec7b6bb94fb276e5e3d77
Kamil Polec <kamilpolec14@gmail.com>
2024-03-11 14:00:33 +0000

    Changed code to be class based

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 from time import time
 
+
 # sentence = "A quick brown fox jumps over the lazy dog."
 
-time_bef = float(0)
+class App(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.title("Typing Speed Test")
 
 
-def calculate_wpm(*args):
-    global time_bef
-    if time_bef == 0:
-        time_bef = float(time())
-    no_of_wrds = (len(sentence.get().split()))
-    if user_sentence.get() == sentence.get():
-        time_aft = (float(time()))
-        wpm = 60 / (time_aft - time_bef) * no_of_wrds
-        wpm_label.set(f"Your WPM is: {wpm:.2f}")
+class Body(ttk.Frame):
+
+    def __init__(self, container):
+        super().__init__()
+        self.grid(pady=12, padx=12, column=0, row=0)
+
+        self.time_bef = float(0)
+
+        self.sentence = tk.StringVar()
+        self.sentence.set(
+            "A quick brown fox jumps over the lazy dog.")
+        ttk.Label(self, textvariable=self.sentence).grid(column=0, row=0)
+
+        self.wpm_label = tk.StringVar()
+        self.wpm_label.set("Please type in the above to calculate your wpm")
+        ttk.Label(self, textvariable=self.wpm_label).grid(column=0, row=1)
+        self.user_sentence = tk.StringVar()
+        self.user_sentence.trace("w", self.calculate_wpm)
+        ttk.Entry(self, textvariable=self.user_sentence).grid(column=0, row=2)
+        ttk.Button(self, text="Start", command=self.calculate_wpm).grid(column=0, row=3)
+
+    def calculate_wpm(self, *args):
+        if self.time_bef == 0:
+            self.time_bef = float(time())
+        no_of_wrds = (len(self.sentence.get().split()))
+        if self.user_sentence.get() == self.sentence.get():
+            time_aft = (float(time()))
+            wpm = 60 / (time_aft - self.time_bef) * no_of_wrds
+            self.wpm_label.set(f"Your WPM is: {wpm:.2f}")
 
 
-root = Tk()
-root.title("Typing speed test")
-
-body = ttk.Frame(root, padding=12)
-body.grid()
-
-sentence = StringVar()
-sentence.set(
-    "A quick brown fox jumps over the lazy dog.")
-ttk.Label(body, textvariable=sentence).grid(column=0, row=0)
-
-wpm_label = StringVar()
-wpm_label.set("Please type in the above to calculate your wpm")
-ttk.Label(body, textvariable=wpm_label).grid(column=0, row=1)
-user_sentence = StringVar()
-user_sentence.trace("w", calculate_wpm)
-ttk.Entry(body, textvariable=user_sentence).grid(column=0, row=2)
-ttk.Button(body, text="Start", command=calculate_wpm).grid(column=0, row=3)
-
-root.mainloop()
+if __name__ == "__main__":
+    app = App()
+    Body(app)
+    app.mainloop()
